84eae71d9956aee2d76459519989655438b83024
furkanguzelant <fguzelant@hotmail.com>
2025-07-28 12:51:47 +0300

    added sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  }
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://furkanguzelant.github.io',
+      routes: [
+        '/', // add more if you have other pages like /about, /publications, etc.
+      ],
+    }),
+  ],
 })
